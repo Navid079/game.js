37acc23acd096fbe9ba2552ef72d40bfcd89b786
Navid079 <navid.naseri.079@gmail.com>
2022-07-26 16:57:10 +0430

    camera object added

--- game.js
+++ game.js
@@ -3,16 +3,23 @@ const Game = (function () {
   const context = canvas.getContext('2d');
   const WIDTH = canvas.width;
   const HEIGHT = canvas.height;
+  const minDim = Math.min(WIDTH, HEIGHT);
+  const maxDim = Math.max(WIDTH, HEIGHT);
+  const ratio = minDim / maxDim;
+  const minLabel = WIDTH < HEIGHT ? 'WIDTH' : 'HEIGHT';
 
-  let id = 0;
+  let componentId = 0;
+  let cameraId = 0;
   let components = [];
+  let cameras = [];
   let tickInterval;
   let renderInterval;
   let isRunning = false;
+  let activeCamera;
 
   class Component {
     constructor(options = {}) {
-      this.id = id++;
+      this.id = componentId++;
       this.logic = this.create();
       this.create = undefined;
       this.options = options;
@@ -32,6 +39,77 @@ const Game = (function () {
     }
   }
 
+  class Camera {
+    constructor(x, y, z) {
+      this.id = cameraId++;
+      this.move(x, y, z);
+      this.getPixelSize();
+      cameras.push(this);
+    }
+
+    getBorder({ cache } = {}) {
+      if (!this.isMoved) return this.border;
+      if (cache) this.isMoved = false;
+      const minOffset = this.z;
+      const maxOffset = Math.round(minOffset / ratio);
+
+      if (minLabel === 'WIDTH') {
+        this.border = [
+          this.x - minOffset,
+          this.y - maxOffset,
+          2 * minOffset,
+          2 * maxOffset,
+        ];
+      } else {
+        this.border = [
+          this.x - maxOffset,
+          this.y - minOffset,
+          2 * maxOffset,
+          2 * minOffset,
+        ];
+      }
+      return this.border;
+    }
+
+    getPixelSize() {
+      if (!this.isMoved) return this.pixelSize;
+      const [x, y, width, height] = this.getBorder({ cache: true });
+      const [xPixels, yPixels] = [width - x, height - y];
+      this.pixelSize = [
+        Math.round(WIDTH / xPixels),
+        Math.round(HEIGHT / yPixels),
+      ];
+
+      return this.pixelSize;
+    }
+
+    move(x, y, z) {
+      this.x = x || this.x;
+      this.y = y || this.y;
+      this.z = z || this.z;
+      this.isMoved = true;
+    }
+
+    isVisible(x, y) {
+      const [sx, sy, width, height] = this.getBorder();
+      if (x < sx || x > sx + width || y < sy || y > sy + height) return false;
+      return true;
+    }
+
+    render(ctx) {
+      for (let component of components) {
+        let { x, y, width, height, sprite } = component.logic;
+        if (this.isVisible(x, y)) {
+          const [pixelWidth, pixelHeight] = this.getPixelSize();
+          width = Math.round(width * pixelWidth);
+          height = Math.round(height * pixelHeight);
+
+          ctx.drawImage(sprite, x, y, width, height);
+        }
+      }
+    }
+  }
+
   function tick() {
     for (let component of components) {
       component.tick();
@@ -40,6 +118,11 @@ const Game = (function () {
 
   function render() {
     context.clearRect(0, 0, WIDTH, HEIGHT);
+
+    if (activeCamera) {
+      return activeCamera.render(context);
+    }
+
     for (let component of components) {
       if (component.options.autoRender) {
         const { sprite, x, y } = component.logic;
@@ -48,6 +131,12 @@ const Game = (function () {
       }
       component.render(context);
     }
+  }
+
+  function setCamera(cameraId) {
+    const foundCamera = cameras.find(camera => camera.id === cameraId);
+    if (!foundCamera) throw new Error('Camera id is invalid');
+    activeCamera = foundCamera;
   }
 
   function start() {
@@ -67,5 +156,7 @@ const Game = (function () {
     Component,
     start,
     stop,
+    Camera,
+    setCamera,
   };
 })();
